3d8d2badc316590f1f737ff123201224e626a2ca
Jeroen Smits <beaker73@live.nl>
2022-06-11 22:55:11 +0200

    timer tests

--- Timer.v
+++ Timer.v
@@ -35,9 +35,42 @@ module Timer(
 endmodule;
 
 module Timer_top(
-  input bit clk, reset
+  input bit clk, reset,
+  output bit isZero
 );
-
+  
+  bit set = 0;
+  bit [15:0] value = 16'd0;
+  
+  Timer timer(
+    .clk(clk), .reset(reset),
+    .set(set), .newValue(value),
+    .isZero(isZero)
+  );
+  
+  bit [15:0] count;
+  always @(posedge clk)
+  begin
+    set <= 0;
+    
+    if (reset) begin
+      value <= 0;
+      count <= 0;
+    end
+    else
+    begin
+      count <= count + 1;
+      
+      // at count 20, set timer to 20
+      // now isZero should become zero for 20 counts
+      if(count == 20)
+      begin
+        set <= 1;
+        value <= 20;
+      end
+    end
+  end
+  
 endmodule;
 
 `endif
